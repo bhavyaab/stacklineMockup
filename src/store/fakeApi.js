7c69764a081e7d6a19222f3a059c8359de2b564e
Bhavya Bharti <bhavyab@outlook.com>
2022-04-25 03:16:00 -0700

    make it better

--- src/store/fakeApi.js
+++ src/store/fakeApi.js
@@ -1,6 +1,6 @@
 
-// var url = 'http://localhost:3000/fakeApi/B007TIE0GQ'; //for local
-var url = 'https://fake-stackline-api.herokuapp.com/B007TIE0GQ';
+var localUrl = 'http://localhost:3000/fakeApi/B007TIE0GQ'; //for local
+var url = 'http://fake-stackline-api.herokuapp.com/B007TIE0GQ' || localUrl;
 
 export async function fakeApiCall(){
      return await fetch(url).then(res => res.json());
