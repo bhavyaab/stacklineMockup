291ecc696654e6e2ee6f0ce6526046468f08f658
Bhavya Bharti <bhavyab@outlook.com>
2022-04-25 03:46:58 -0700

    route fixed

--- src/store/fakeApi.js
+++ src/store/fakeApi.js
@@ -1,6 +1,6 @@
 
 var localUrl = 'http://localhost:8080/fakeApi/B007TIE0GQ'; //for local
-var url = 'http://fake-stackline-api.herokuapp.com/B007TIE0GQ';
+var url = 'http://fake-stackline-api.herokuapp.com/fakeApi/B007TIE0GQ';
 
 export async function fakeApiCall(){
      return await fetch(url).then(res => res.json());
